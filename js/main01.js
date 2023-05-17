dfde8a76356fa196ddf1bd372d4769fddb74e2cd
jinjin121 <110872022+jinjin121@users.noreply.github.com>
2023-05-17 16:44:28 +0900

    Add files via upload

---
+++ js/main01.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.mainSlide').slick({
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnHover: false,
+        // 마우스를 올려도 슬라이드가 자동으로 넘어가도록 한다
+
+    });
+})
